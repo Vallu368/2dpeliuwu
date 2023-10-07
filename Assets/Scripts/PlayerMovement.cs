5af46f042fafa18d2fc7bd48656a8bb80a41b5a0
Vallu368 <vsavolainen22@gmail.com>
2023-10-08 01:43:04 +0300

    ui fuckery, keep game at 1280x720 or ui breaks cus im fucking stupid

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f; // Adjust the speed as needed
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = 0f;
+        float verticalInput = 0f;
+
+        // Check for key presses and release
+        if (Input.GetKey(KeyCode.W))
+        {
+            verticalInput = 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            verticalInput = -1f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            horizontalInput = 1f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            horizontalInput = -1f;
+        }
+
+        Vector2 moveDirection = new Vector2(horizontalInput, verticalInput).normalized;
+
+        // Apply movement to the Rigidbody2D
+        rb.velocity = moveDirection * moveSpeed;
     }
 }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class PlayerStats : MonoBehaviour
@@ -8,11 +9,31 @@ public class PlayerStats : MonoBehaviour
     public float battlePower;
     public float energy;
 
+    public float maxHealth;
+    public float currentHealth;
+    public GameObject healthBar;
+
     public TextMeshProUGUI bpNumber;
     public TextMeshProUGUI enNumber;
 
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
     public void Update()
     {
+        healthBar.GetComponent<Slider>().maxValue = maxHealth;
+        healthBar.GetComponent<Slider>().value = currentHealth;
+
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+
         bpNumber.text = battlePower.ToString();
         enNumber.text = energy.ToString();
     }
